45d0ae28ec165e8e9dd875cdc211f072f7c92b9b
Miriam <miriamgaviria2@gmail.com>
2021-04-14 20:00:27 +0200

    Add - logo in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import Images from '../../../assets/imagesUrl.json';
 import Texts from '../../../assets/texts.json';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  images: any = Images;
   texts: any = Texts;
 
   loggedUserId: string;

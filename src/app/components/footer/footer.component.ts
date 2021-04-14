4f140e75179cf40385cd4024e9aff584d85b7add
Miriam <miriamgaviria2@gmail.com>
2021-04-14 20:26:56 +0200

    Add - footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import Images from '../../../assets/imagesUrl.json';
+import Texts from '../../../assets/texts.json';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  images: any = Images;
+  texts: any = Texts;
 
   public author: any = {name: 'TV-LIX', company: 'Gaviros Company S.A.'};
 
